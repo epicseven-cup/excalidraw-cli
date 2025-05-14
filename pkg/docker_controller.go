f58745c2daf278eb590890a8e29e93d603dfe0b1
epicsven-cup <jackylin616@gmail.com>
2025-05-13 23:14:28 -0400

    some smaller updates

--- pkg/docker_controller.go
+++ pkg/docker_controller.go
@@ -8,9 +8,8 @@ import (
 	"github.com/docker/docker/client"
 )
 
-
-// Beacuse of the differences between `podman` and `docker`, in `docker` you will need to keep track of the container `id`
-// Where in podman you can choose to keep track of `id` or container `name`
+// DockerController is the main engine controller that will be used to control the excalidraw container
+// Engine -> The value that determine which engine the controller is for
 type DockerController struct {
 	client *client.Client
 	Engine ContainerEngine
@@ -28,35 +27,26 @@ func NewDockerController(system string) (*DockerController, error) {
 	}, nil
 }
 
-func (dc *DockerController) status(name string) (bool, error) {
-	clientList, err := dc.client.ContainerList(context.Background(), container.ListOptions{})
+func (dc *DockerController) exist(imageName string) (bool, error) {
+	containerList, err := dc.client.ContainerList(context.Background(), container.ListOptions{})
 	if err != nil {
 		return false, err
 	}
-	for _, c := range clientList {
-		fmt.Println(c.Names)
-		if c.Names[0] == name {
+	for _, i := range containerList {
+		if i.Image == imageName {
 			return true, nil
 		}
 	}
 	return false, nil
 }
 
-func (dc *DockerController) exist(name string) (bool, error) {
-	imageList, err := dc.client.ImageList(context.Background(), image.ListOptions{})
-	if err != nil {
-		return false, err
+func (dc *DockerController) run(imageName string, name string) error {
+	config := &container.Config{
+		Image: imageName,
 	}
-	for _, i := range imageList {
-		fmt.Println(i)
-	}
-	return false, nil
-}
-
-func (dc *DockerController) run(name string) error {
 	create, err := dc.client.ContainerCreate(
 		context.Background(),
-		&container.Config{Image: name},
+		config,
 		nil,
 		nil,
 		nil,
@@ -74,6 +64,28 @@ func (dc *DockerController) run(name string) error {
 	return nil
 }
 
+func (dc *DockerController) stop(name string) error {
+	err := dc.client.ContainerStop(context.Background(), name, container.StopOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (dc *DockerController) status(name string) (bool, error) {
+	clientList, err := dc.client.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		return false, err
+	}
+	for _, c := range clientList {
+		fmt.Println(c.Names)
+		if c.Names[0] == name {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
 func (dc *DockerController) update(name string) error {
 	pull, err := dc.client.ImagePull(context.Background(), name, image.PullOptions{})
 	if err != nil {
@@ -81,14 +93,6 @@ func (dc *DockerController) update(name string) error {
 	}
 	fmt.Println("Image pulled")
 	err = pull.Close()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (dc *DockerController) exit(name string) error {
-	err := dc.client.ContainerRemove(context.Background(), name, container.RemoveOptions{})
 	if err != nil {
 		return err
 	}
